2cf8de4cb210aad8d92bacf34f85f26d0e251a18
AdamDavisDeveloper <adavisdeveloper@gmail.com>
2023-10-31 02:48:28 -0400

    begin official reskin and making this project real

---
+++ src/Components/PlaylistNavigator.tsx
@@ -0,0 +1,12 @@
+// Controls which playlist is in view. (Will effect parent component state)
+// Left and right image arrows allow user to flip through playlists
+// Playlist title appear between the left and right arrow buttons
+
+function PlaylistNavigator(props:any) {
+    props = props;
+    return (
+        <></>
+    )
+};
+
+export default PlaylistNavigator;
---
+++ src/MainPlayer.tsx
@@ -0,0 +1,27 @@
+//import { useRef, useState } from 'react';
+import './App.css' 
+import './Main.scss'; //@ts-ignore
+
+function MainPlayer() {
+    return (
+        <>
+        
+        {/* Top Frame */}
+
+        {/* Header */}
+
+        {/* PlaylistNavigator */}
+
+        {/* Record Table / Cover Art */}
+
+        {/* Track List */}
+
+        {/* Bottom Frame */}
+
+        {/* Music Player */}
+
+        </>
+    )
+}
+
+export default MainPlayer;
