8345cc834d4c8127b3bb0be985103ae3b5d125c5
AdamDavisDeveloper <adavisdeveloper@gmail.com>
2023-11-07 02:51:51 -0500

    Much progress on Nav, Cover art, Track list + scss

--- src/MainPlayer.tsx
+++ src/MainPlayer.tsx
@@ -1,9 +1,46 @@
 import { useState } from 'react';
+import LeftArrow from './assets/LeftArrowBlack.svg'
+import RightArrow from './assets/RightArrowBlack.svg'
+import CoverTest from './assets/cover-test.png'
 import './Scss/MainPlayer.scss';
+
+
+const TrackList = [
+    {
+        title: "f a r a w a y",
+        artist: "The Japanese House"
+    },
+    {
+        title: "Dancing with my phone",
+        artist: "HYBS"
+    },
+    {
+        title: "Anything",
+        artist: "SOMOH"
+    },
+    {
+        title: "no way out",
+        artist: "ivri"
+    },
+]
 
 function MainPlayer() {
     //@ts-ignore
     const [ currentPlaylist, setCurrentPlaylist ] = useState("Spontaneous Combustion");
+
+    const TrackSelector = () => {
+        const tracks = TrackList.map((track) => {
+            return (
+                <>
+                <div className="track">
+                    <button> {track.title} - {track.artist} </button>
+                </div>
+                </>
+            )
+        });
+
+        return <div id="AllTracks">{ tracks }</div>;
+    };
 
     return (
         <div id="MainPlayer">
@@ -16,20 +53,29 @@ function MainPlayer() {
 
             {/* Header */}
             <header>
-                <div className="top"><h1>ADAM DAVIS</h1></div>
+                <div className="top"><h1>ADAMS LAND</h1></div>
                 <div className="bottom"><h2>JUKEBOX</h2></div>
             </header>
 
             {/* PlaylistNavigator */}
             <div className="line"></div>
             <div id="PlaylistNav">
-                <div className="title">{currentPlaylist}</div>
+                <button><img src={LeftArrow} alt="Left arrow button" /></button>
+                <div className="title"><h3>{currentPlaylist}</h3></div>
+                <button><img src={RightArrow} alt="Right arrow button" /></button>
             </div>
             <div className="line"></div>
 
             {/* Record Table / Cover Art */}
+            <div id="CoverArt">
+                <img src={CoverTest} alt="Playlist cover art" />
+            </div>
 
             {/* Track List */}
+            <h4>Title</h4>
+            <div id="TrackList">
+                { TrackSelector() }
+            </div>
 
             {/* Bottom Frame */}
 
